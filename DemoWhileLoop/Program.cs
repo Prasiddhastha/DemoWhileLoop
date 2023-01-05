60b21b0374ce0dfae7de0f6169a922db45640502
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2023-01-05 15:00:24 +0545

    Add project files.

---
+++ DemoWhileLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //whileloop
+            var i=0;
+            while(i<10)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            Console.WriteLine("\n");
+            //dowhileloop
+            var j = 0;
+            do
+            {
+
+                if (j % 2 == 0)
+                {
+                    Console.WriteLine(j);
+                }
+                j++;
+            }while(j<10);
+        }
+    }
+}
